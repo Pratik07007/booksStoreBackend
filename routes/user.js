5d29f6b872425ec7fb0a24df6aec5a521d2a5e02
Pratik07007 <s.dhimal006@gmail.com>
2024-04-22 12:18:14 +0545

    Purchased course get feature implemented

--- routes/user.js
+++ routes/user.js
@@ -2,8 +2,9 @@ const express = require("express");
 const userRegistrationMiddleware = require("../middlewares/userRegistrationMiddleware");
 const userValidiationMiddleware = require("../middlewares/userValidiationMiddleware");
 const userAuthenticationMiddleware = require("../middlewares/userAuthenticationMiddleware");
-
 const bookPurchaseMiddleware = require("../middlewares/bookPurchaseMiddleware");
+const jwt = require("jsonwebtoken");
+const { UserSchema } = require("../Dbs");
 
 const app = express();
 
@@ -22,5 +23,21 @@ app.post(
 );
 
 app.post("/user/purchased/:bookId", bookPurchaseMiddleware);
+
+app.get("/user/purchasedCourse", async (req, res) => {
+  try {
+    const token = req.headers.authorization;
+    const response = jwt.verify(token, "12345");
+    if (response.email) {
+      const user = await UserSchema.findOne({ email: response.email });
+      if (user) {
+        res.status(200).json(user.purchasedBooks);
+      } else {
+      }
+    }
+  } catch (err) {
+    res.json({ err });
+  }
+});
 
 app.listen(3000);
